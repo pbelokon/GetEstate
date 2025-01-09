276fbde79511e5e220de7366d037eb266ddf54b5
Pavel Belokon <pavelbelokon.pb@gmail.com>
2025-01-09 16:00:56 -0500

    clerk version bump down

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider dynamic>
+    <ClerkProvider>
       <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
