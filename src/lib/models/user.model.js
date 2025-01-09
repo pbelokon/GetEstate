8d4c9c1cc9b030162636fd60c7f9ba61dd350361
Pavel Belokon <pavelbelokon.pb@gmail.com>
2025-01-09 14:45:43 -0500

    imports and boiler plate added back

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 const userSchema = new mongoose.Schema(
   {
     clerkId: {
@@ -26,5 +27,7 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
 const User = mongoose.models.User || mongoose.model('User', userSchema);
+
 export default User;
